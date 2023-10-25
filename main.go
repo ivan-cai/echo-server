5a006d816769832e32f6eade4ab1bbe59e819a67
ivanscai <caijing.cai@alibaba-inc.com>
2023-10-25 14:58:04 +0800

    Update main.go

--- main.go
+++ main.go
@@ -12,7 +12,7 @@ func main() {
 	router := mux.NewRouter()
 
 	router.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		logs.Infof("Hello Echo Server")
+		logs.Infof("Hello Echo Server for main")
 		json.NewEncoder(w).Encode("Hello Echo Server")
 	})
 
